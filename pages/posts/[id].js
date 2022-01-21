fc4f867af5d584e737464619e0c721ff2e431100
Kim DongHyun <shingidad@gmail.com>
2022-01-21 12:03:26 +0900

    first commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Head from "next/head";
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import utilStyles from "../../styles/utils.module.css";
+
+export async function getStaticProps({ params }) {
+  console.log("getStaticProps");
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  console.log("getStaticPaths");
+  const paths = getAllPostIds();
+  console.log("🚀 ~ file: [id].js ~ line 19 ~ getStaticPaths ~ paths", paths)
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
